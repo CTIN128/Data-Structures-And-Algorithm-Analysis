506689926b297b223d53d2080267f0b910cc8e44
CTIN128 <ctinson128@gmail.com>
2016-09-13 20:51:33 +0800

    CommitInfo

--- Unit1-2/Sort/shellsort.cpp
+++ Unit1-2/Sort/shellsort.cpp
@@ -1,33 +1,33 @@
-//#include <iostream>
-//
-//void ShellSort(int arr[], int length)
-//{
-//	int i, j, middle = length, temp;
-//	for (middle >>= 1; middle > 0; middle >>= 1)
-//	{
-//		for (i = middle; i < length; ++i)
-//		{
-//			temp = arr[i];
-//			for (j = i; j >= middle; j -= middle)
-//			{
-//				if (temp < arr[j - middle])
-//				{
-//					arr[j] = arr[j - middle];
-//				}
-//				else
-//					break;
-//			}
-//			arr[j] = temp;
-//		}
-//
-//	}
-//}
-//int main()
-//{
-//	int data[6] = { 1,8,7,9,1,4 };
-//	ShellSort(data, 6);
-//	for (auto x : data)
-//	{
-//		std::cout << x << std::endl;
-//	}
-//}
+#include <iostream>
+
+void ShellSort(int arr[], int length)
+{
+	int i, j, middle = length, temp;
+	for (middle >>= 1; middle > 0; middle >>= 1)
+	{
+		for (i = middle; i < length; ++i)
+		{
+			temp = arr[i];
+			for (j = i; j >= middle; j -= middle)
+			{
+				if (temp < arr[j - middle])
+				{
+					arr[j] = arr[j - middle];
+				}
+				else
+					break;
+			}
+			arr[j] = temp;
+		}
+
+	}
+}
+int main()
+{
+	int data[6] = { 1,8,7,9,1,4 };
+	ShellSort(data, 6);
+	for (auto x : data)
+	{
+		std::cout << x << std::endl;
+	}
+}
