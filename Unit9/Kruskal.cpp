7d81d953d2bcf51f94f095f155874aca2769553e
CTIN128 <ctinson128@gmail.com>
2016-12-21 11:00:09 +0800

    Fix Some bug

--- Unit9/Kruskal.cpp
+++ Unit9/Kruskal.cpp
@@ -1,69 +1,77 @@
-#include <queue>
-#include <vector>
-#include <iostream>
-#include <limits>
+/*
+Kruskal思想：
+将所有边都压入最小堆中，然后一一弹出，但是此时要判断是否构成回环，可以这样判断
+先建立个数组，数组下标即为该点编号，数组里的值先为下标值，如果1与3连，那么array[3] = 1,1与4连，
+那么array[4] = 1，那么3与4就不能相连
+*/
 
-struct VertexInfo
-{
-	VertexInfo() {}
-	VertexInfo(int _Number, int _Dist):Number(_Number), Close_Dist(_Dist) {}
-	bool Known = false;
-	int Number;
-	int Close_Dist = INT_MAX;
-	int Path;
-};
-struct Vertex
-{
-	Vertex(int &_Start, int &_End,int &_Dist) :Start(_Start), End(_End),Dist(_Dist){}
-    int Start,End,Dist;
-};
-bool operator< (Vertex p1, Vertex p2)//此处有坑不准加上引用
-{
-	return p1.Dist > p2.Dist;
-}
-int main()
-{
-	std::priority_queue<Vertex> pq;
-	std::vector<VertexInfo> vec(7);
-	int Graph[][7] =//创建无向图，但是不相邻的节点要用INT_MAX代替
-	{
-		0,2,4,1,INT_MAX,INT_MAX,INT_MAX,
-		2,0,INT_MAX,3,10,INT_MAX,INT_MAX,
-		4,INT_MAX,0,2,INT_MAX,5,INT_MAX,
-		1,3,2,0,7,8,4,
-		INT_MAX,10,INT_MAX,7,0,INT_MAX,6,
-		INT_MAX,INT_MAX,5,8,INT_MAX,0,1,
-		INT_MAX,INT_MAX,INT_MAX,4,6,1,0
-	};
-	for (int i = 0; i < 7; ++i)
-	{
-		for (int j = 6; j >= i; --j)//取哪一半都一样
-		{
-			if(Graph[i][j]!=INT_MAX && i!=j)
-			pq.push(Vertex(i, j, Graph[i][j]));
-		}
-	}
-	int judge[7];
-	for (int i = 0; i < 7; ++i)
-	{
-		judge[i] = i;
-	}
-	while (!pq.empty())
-	{
-		Vertex tmpcell = pq.top();
-		pq.pop();
-		int Sn1 = judge[tmpcell.Start];
-		int Sn2 = judge[tmpcell.End];
-		if (Sn1 != Sn2)
-		{
-			std::cout << "("<< tmpcell.Start <<", " << tmpcell.End << ")" << std::endl;
-			for (int i = 0; i < 7; ++i)
-			{
-				if (judge[i] == Sn2)
-				{
-					judge[i] = Sn1;
-				}
-			}
-		}
-	}
-}
+
+//#include <queue>
+//#include <vector>
+//#include <iostream>
+//#include <limits>
+//
+//struct VertexInfo
+//{
+//	VertexInfo() {}
+//	VertexInfo(int _Number, int _Dist):Number(_Number), Close_Dist(_Dist) {}
+//	bool Known = false;
+//	int Number;
+//	int Close_Dist = INT_MAX;
+//	int Path;
+//};
+//struct Vertex
+//{
+//	Vertex(int &_Start, int &_End,int &_Dist) :Start(_Start), End(_End),Dist(_Dist){}
+//    int Start,End,Dist;
+//};
+//bool operator< (Vertex p1, Vertex p2)//此处有坑不准加上引用
+//{
+//	return p1.Dist > p2.Dist;
+//}
+//int main()
+//{
+//	std::priority_queue<Vertex> pq;
+//	std::vector<VertexInfo> vec(7);
+//	int Graph[][7] =//创建无向图，但是不相邻的节点要用INT_MAX代替
+//	{
+//		0,2,4,1,INT_MAX,INT_MAX,INT_MAX,
+//		2,0,INT_MAX,3,10,INT_MAX,INT_MAX,
+//		4,INT_MAX,0,2,INT_MAX,5,INT_MAX,
+//		1,3,2,0,7,8,4,
+//		INT_MAX,10,INT_MAX,7,0,INT_MAX,6,
+//		INT_MAX,INT_MAX,5,8,INT_MAX,0,1,
+//		INT_MAX,INT_MAX,INT_MAX,4,6,1,0
+//	};
+//	for (int i = 0; i < 7; ++i)
+//	{
+//		for (int j = 6; j >= i; --j)//取哪一半都一样
+//		{
+//			if(Graph[i][j]!=INT_MAX && i!=j)
+//			pq.push(Vertex(i, j, Graph[i][j]));
+//		}
+//	}
+//	int judge[7];
+//	for (int i = 0; i < 7; ++i)
+//	{
+//		judge[i] = i;
+//	}
+//	while (!pq.empty())
+//	{
+//		Vertex tmpcell = pq.top();
+//		pq.pop();
+//		int Sn1 = judge[tmpcell.Start];
+//		int Sn2 = judge[tmpcell.End];
+//		if (Sn1 != Sn2)
+//		{
+//			std::cout << "(" << tmpcell.Start << ", " << tmpcell.End << ") "  << tmpcell.Dist<< std::endl;
+//			for (int i = 0; i < 7; ++i)
+//			{ 
+//				if (judge[i] == Sn2)
+//				{
+//					judge[i] = Sn1;
+//				}
+//			}
+//		}
+//	}
+//}
--- Unit9/Prim.cpp
+++ Unit9/Prim.cpp
@@ -1,3 +1,10 @@
+/*
+  Prim思想：
+  先确定一个源点，设为已知点，作为集合A，则剩下的未知点即为集合B，从源点出发，
+  将与之相邻并且未知的点压入最小堆（以权值大小排序）中，然后从堆中弹出权值最小的点m，此时A中
+  加入该点m，B中删除该点m。同理接着就把与该点相邻并且未知的点压入最小堆中，弹出权值最小的点n，
+  此时A中加入该点n，B中删除n。一直这样后，直到所有点都被访问
+*/
 #include <queue>
 #include <iostream>
 #include <vector>
@@ -15,47 +22,52 @@ bool operator<(Vertex p1, Vertex p2)
 {
 	return p1.Close_Dist > p2.Close_Dist;
 }
-void PrintPath(int SourceVertex, int vertex, std::vector<Vertex> v)
-{
-	if (vertex == SourceVertex)
-	{
-		std::cout << vertex << std::endl;
-		return;
-	}
-	std::cout << vertex << std::endl;
-	PrintPath(SourceVertex, v[vertex].Path, v);
-}
 int main()
 {
-	int SourceVetex = 6;
+	int SourceVetex = 4;
 	std::vector<Vertex> vec(7);
 	vec[SourceVetex].Close_Dist = 0;
 	vec[SourceVetex].Path = SourceVetex;
 	std::priority_queue<Vertex> pq;
+	std::vector<bool> Known(7,false);
+	//int Graph[][7] =
+	//{
+	//	0,2,4,1,INT_MAX,INT_MAX,INT_MAX,
+	//	2,0,INT_MAX,3,10,INT_MAX,INT_MAX,
+	//	4,INT_MAX,0,2,INT_MAX,5,INT_MAX,
+	//	1,3,2,0,7,8,4,
+	//	INT_MAX,10,INT_MAX,7,0,INT_MAX,6,
+	//	INT_MAX,INT_MAX,5,8,INT_MAX,0,1,
+	//	INT_MAX,INT_MAX,INT_MAX,4,6,1,0
+	//};
 	int Graph[][7] =
 	{
-		0,2,4,1,INT_MAX,INT_MAX,INT_MAX,
-		2,0,INT_MAX,3,10,INT_MAX,INT_MAX,
-		4,INT_MAX,0,2,INT_MAX,5,INT_MAX,
-		1,3,2,0,7,8,4,
-		INT_MAX,10,INT_MAX,7,0,INT_MAX,6,
-		INT_MAX,INT_MAX,5,8,INT_MAX,0,1,
-		INT_MAX,INT_MAX,INT_MAX,4,6,1,0
+		0,8,INT_MAX,INT_MAX,6,INT_MAX,INT_MAX,
+		8,0,4,7,INT_MAX,INT_MAX,INT_MAX,
+		INT_MAX,4,0,INT_MAX,INT_MAX,2,INT_MAX,
+		INT_MAX,7,INT_MAX,0,8,1,INT_MAX,
+		6,INT_MAX,INT_MAX,8,0,INT_MAX,7,
+		INT_MAX,INT_MAX,2,1,INT_MAX,0,5,
+		INT_MAX,INT_MAX,INT_MAX,INT_MAX,7,5,0
 	};
 	pq.push(Vertex(SourceVetex, 0));
-	while (!pq.empty())
+	int IsKnown = 0;
+	while (!pq.empty()&&IsKnown != 7)
 	{
 		Vertex tmpcell = pq.top();
 		pq.pop();
-		vec[tmpcell.Number].Known = true;
+		Known[tmpcell.Number] = true;
+		++IsKnown;//判断是否都已访问过
 		for (int i = 0; i < 7; ++i)
 		{
-			if (i != tmpcell.Number&&!vec[i].Known&&Graph[tmpcell.Number][i] < vec[i].Close_Dist)
+			if (Graph[tmpcell.Number][i] != INT_MAX&&Graph[tmpcell.Number][i] != 0 && !Known[i])
 			{
-				vec[i].Path = tmpcell.Number;
-				vec[i].Close_Dist = Graph[tmpcell.Number][i];
-				pq.push(Vertex(i, vec[i].Close_Dist));
-
+				pq.push(Vertex(i, Graph[tmpcell.Number][i]));
+				if (Graph[tmpcell.Number][i] < vec[i].Close_Dist)//如果tmpcell.number点到i的值比原来close_dist的小，则更新。
+				{                                                //此时close_dist不是该点到源点距离而是该点到离他最近点的距离
+					vec[i].Close_Dist = Graph[tmpcell.Number][i];
+					vec[i].Path = tmpcell.Number;
+				}
 			}
 		}
 	}
